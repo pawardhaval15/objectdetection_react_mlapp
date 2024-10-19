3c8a3a9bf70a5cb1b2da637e510ec3df040099cf
Dhaval Pawar <89838369+pawardhaval15@users.noreply.github.com>
2024-10-20 00:53:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/footer';
+import ObjectDetection from './ObjectDetection';
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Xo Object Detection</h1>
+        <ObjectDetection />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';  // Make sure this imports your CSS
+
+const Footer = () => {
+  return (
+    <footer className="footer-section">
+      <h3>WEB REACT MLKIT</h3>
+      <h4>XO_OBJECT_DETECTION MODEL</h4>
+      <p>© 2024 Dhaval.</p>
+    </footer>
+  );
+};
+
+export default Footer;
