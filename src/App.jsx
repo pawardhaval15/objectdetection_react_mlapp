8e48d39ca9f4a24fbfa65f9af92fd37bcddeb7f2
Dhaval Pawar <pawardhaval15@gmail.com>
2024-10-19 23:51:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ObjectDetection from './ObjectDetection';
+function App() {
+  return (
+    <div className="app">
+      <h1>Xo Object Detection</h1>
+        <ObjectDetection />
+    </div>
+  );
+}
+
+export default App;
